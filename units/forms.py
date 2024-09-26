199a11a34c8edf8aeebab23a7fd4bce7430f8a9b
Yusha2849 <myusha2849@gmail.com>
2024-09-27 00:00:57 +0200

    Added a drafted version of Secretary functionality

--- units/forms.py
+++ units/forms.py
@@ -47,3 +47,52 @@ class AddSecretaryForm(FlaskForm):
         Regexp(r'^\d{10}$', message='Cell number must be exactly 10 digits')
     ])
     submit = SubmitField('Add Secretary')
+
+class AddParentForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=50)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=50)])
+    rsa_id_num = StringField('RSA ID Number', validators=[DataRequired(), Length(min=13, max=13)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    cell_number = StringField('Cell Number', validators=[DataRequired(), Length(min=10, max=10)])
+
+    street_address = StringField('Street Address', validators=[DataRequired(), Length(min=5, max=100)])
+    suburb = StringField('Suburb', validators=[DataRequired(), Length(min=2, max=50)])
+    city = StringField('City', validators=[DataRequired(), Length(min=2, max=50)])
+    province = SelectField('Province', choices=[
+        ('Gauteng', 'Gauteng'),
+        ('Western Cape', 'Western Cape'),
+        ('KwaZulu-Natal', 'KwaZulu-Natal'),
+        ('Eastern Cape', 'Eastern Cape'),
+        ('Free State', 'Free State'),
+        ('Limpopo', 'Limpopo'),
+        ('Mpumalanga', 'Mpumalanga'),
+        ('Northern Cape', 'Northern Cape'),
+        ('North West', 'North West')
+    ], validators=[DataRequired()])
+
+    submit = SubmitField('Add Parent')
+
+
+class AddStudentForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=50)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=50)])
+    id_num = StringField('ID Number', validators=[DataRequired(), Length(min=13, max=13)])
+    guardian = SelectField('Guardian', choices=[], coerce=int, validators=[DataRequired()])
+    grade = SelectField('Grade', choices=[], validators=[DataRequired()])
+    division = SelectField('Division', choices=[], validators=[DataRequired()])
+    submit = SubmitField('Add Student')
+
+class ExemptionForm(FlaskForm):
+    start_date = DateField('Start Date', validators=[DataRequired()])
+    end_date = DateField('End Date', validators=[DataRequired()])
+    grade = SelectField('Grade', choices=[], validators=[DataRequired()])
+    division = SelectField('Division', choices=[], validators=[DataRequired()])
+    student_id = SelectField('Student ID', choices=[], validators=[DataRequired()])
+    reason = TextAreaField('Reason for Exemption', validators=[DataRequired()])
+    submit = SubmitField('Update Attendance')
+
+class GenerateClassListForm(FlaskForm):
+    date = DateField('Date', validators=[DataRequired()])
+    grade = SelectField('Grade', choices=[], validators=[DataRequired()])
+    division = SelectField('Division', choices=[], validators=[DataRequired()])
+    submit = SubmitField('Generate List')
